95be7218f88a315df22c026248d2961af02d8f3b
hadesfranklyn <franklyn1998@live.com>
2023-03-21 10:34:52 -0300

    Projeto criado

---
+++ src/main/java/com/github/hadesfranklyn/project/HelloDockerApplication.java
@@ -0,0 +1,13 @@
+package com.github.hadesfranklyn.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/hadesfranklyn/project/HelloDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hadesfranklyn.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
